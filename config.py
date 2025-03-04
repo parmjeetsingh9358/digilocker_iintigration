a12a932ddbfb65be4cbc079e915cc91b75ecb171
parmjeet.singh <parmjeet.singh@dpdpconsultants.com>
2025-03-04 17:04:28 +0530

    [IMP] Update config file with new credentials for DigiLocker integration

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
-CLIENT_ID = "VL558E2260"
-CLIENT_SECRET = "b635a872f2ce72667ea0"
+CLIENT_ID = "HMBAEBFEE0"
+CLIENT_SECRET = "b48dd4cbb56a06cb2e03"
 REDIRECT_URI = "https://testing.dpdp-privcy.in.net/callback"
 
-SETU_CLIENT_ID = "VL558E2260"
-SETU_CLIENT_SECRET = "b635a872f2ce72667ea0"
+SETU_CLIENT_ID = "HMBAEBFEE0"
+SETU_CLIENT_SECRET = "b48dd4cbb56a06cb2e03"
 SETU_PRODUCT_INSTANCE_ID = "891707ee-d6cd-4744-a28d-058829e30f10"
 
 AUTH_URL = "https://digilocker.meripehchaan.gov.in/public/oauth2/1/authorize"

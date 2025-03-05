026a7582b4de25534f361cfb231e055bdb510313
parmjeet.singh <parmjeet.singh@dpdpconsultants.com>
2025-03-05 15:48:14 +0530

    Update my conf file

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
-CLIENT_ID = "CLIENT_ID"
-CLIENT_SECRET = "CLIENT_SECRET"
+CLIENT_ID = "HMBAEBFEE0"
+CLIENT_SECRET = "b48dd4cbb56a06cb2e03"
 REDIRECT_URI = "https://testing.dpdp-privcy.in.net/callback"
 
-SETU_CLIENT_ID = "CLIENT_ID"
-SETU_CLIENT_SECRET = "CLIENT_SECRET"
+SETU_CLIENT_ID = "HMBAEBFEE0"
+SETU_CLIENT_SECRET = "b48dd4cbb56a06cb2e03"
 SETU_PRODUCT_INSTANCE_ID = "891707ee-d6cd-4744-a28d-058829e30f10"
 
 AUTH_URL = "https://digilocker.meripehchaan.gov.in/public/oauth2/1/authorize"
